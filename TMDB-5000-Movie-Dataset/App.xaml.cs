3e3f6721004194f2b9312847366d06724dd9b174
например <@LENOVO-PC>
2017-10-11 18:36:32 +0500

    Добавьте файлы проекта.

---
+++ TMDB-5000-Movie-Dataset/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TMDB_5000_Movie_Dataset
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
