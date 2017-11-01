09f94f54114f0d186dcd8b5e5345e885d37584f4
например <@LENOVO-PC>
2017-11-01 15:53:38 +0500

    Добавлен  класс для классификатора

---
+++ TMDB-5000-Movie-Dataset/Classifier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMDB_5000_Movie_Dataset
+{
+    class Classifier
+    {
+        List<string> genres = new List<string>();
+        List<string> keywords = new List<string>();
+        int[] frequencies;
+        Classifier() { }
+    }
+}
--- TMDB-5000-Movie-Dataset/MainWindow.xaml.cs
+++ TMDB-5000-Movie-Dataset/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,14 @@ namespace TMDB_5000_Movie_Dataset
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if(!File.Exists("data.csv"))
+            {
+                ////////
+            }
         }
     }
 }
